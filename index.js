50f7fae1ad2fa479141339962c41eb4e895e0421
Matt <matthew.salt@gmail.com>
2017-02-17 18:08:41 +0000

    tidy up

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-function openLink(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
+function openLink (info, tab) {
   var url = info.linkUrl
-  console.log("New URL:" +  url)
-  chrome.tabs.duplicate(tab.id, function(newTab){
-    console.log('NEW TAB ' + newTab.id)
-    console.log(newTab)
-    chrome.tabs.update(newTab.id,{url:url})
+  chrome.tabs.duplicate(tab.id, function (newTab) {
+    chrome.tabs.update(newTab.id, {url: url})
   })
 }
 
-var context = "link"
-var title = "Open in duplicate tab";
-var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                       "onclick": openLink});
-console.log("'" + context + "' item:" + id);
-
+chrome.contextMenus.create({
+  'title': 'Open in duplicate tab',
+  'contexts': ['link'],
+  'onclick': openLink
+}
+)
